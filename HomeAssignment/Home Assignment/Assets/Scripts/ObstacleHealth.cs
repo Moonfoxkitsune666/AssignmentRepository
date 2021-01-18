143186aeba0cc1c9ebbe5b8a9ab5f884d5d1dc72
Moonfoxkitsune666 <73173359+Moonfoxkitsune666@users.noreply.github.com>
2021-01-18 18:30:50 +0100

    3rd commit
    Fixed the spawner and filed the sprites

---
+++ HomeAssignment/Home Assignment/Assets/Scripts/ObstacleHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] AudioClip explosionSound;
+    [SerializeField] [Range(0, 1)] float explosionSoundVolume = 0.7f;
+    [SerializeField] GameObject Death;
+    [SerializeField] float explosionDuration = 1f;
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        DamageDealer dmgDealer = otherObject.gameObject.GetComponent<DamageDealer>();
+        if (!dmgDealer)
+        {
+            return;
+        }
+        RegisterHit(dmgDealer);
+    }
+
+    private void RegisterHit(DamageDealer dmgDealer)
+    {
+        health -= dmgDealer.GetDamage();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(Death, transform.position, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, explosionSoundVolume);
+        Destroy(explosion, explosionDuration);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
